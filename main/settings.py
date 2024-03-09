2aaa85383d9bfb96063386e96245b6b38abc4d3e
Mansur Fayziev <114176073+mansurfayziev@users.noreply.github.com>
2024-03-09 17:34:29 +0700

    allowed hosting

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k_p#t57w8pn-w8@0lilw^rlg=wuo5rrqi(*zm)kb^eo+b#__o6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
